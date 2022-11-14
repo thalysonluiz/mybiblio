31944441baf949cb3623525c41a63accb856b916
Thalyson Luiz <thalysonluiz@gmail.com>
2022-11-14 16:39:17 -0300

    update design import page

--- resources/views/admin/books/import.blade.php
+++ resources/views/admin/books/import.blade.php
@@ -1,74 +1,102 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Import') }}
+        <h2 class="font-semibold text-2xl text-white leading-tight">
+            Importar Livros
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl px-2">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class=" flex justify-between gap-3">
+                <div
+                    class="overflow-hidden shadow-sm sm:rounded-lg flex-1 min-h-[200px] p-6 bg-white border-b border-gray-200">
 
-        <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <h2 class="text-2xl font-bold mb-7 dark:text-white">Importar Planilha de Livros</h2>
+                    <div class="card card-flush h-xl-100">
+                        <!--begin::Card header-->
+                        <div class="flex justify-between items-center p-2">
 
-            <form method="post" action="{{ route('admin.importarExcel') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="my-4">
-                    <label @class([
-                        'block',
-                        'mb-2',
-                        'text-sm',
-                        'font-medium',
-                        'dark:text-gray-300' => !$errors->has('file'),
-                        'text-gray-900' => !$errors->has('file'),
-                        'text-red-700' => $errors->has('file'),
-                        'dark:text-red-500' => $errors->has('file'),
-                    ]) for="user_avatar">Upload
-                        file</label>
-                    <input @class([
-                        'block',
-                        'w-full',
-                        'text-sm',
-                        'bg-gray-50',
-                        'rounded-lg',
-                        'border',
-                        'text-sm',
-                        'bg-gray-50' => !$errors->has('file'),
-                        'border-gray-300' => !$errors->has('file'),
-                        'cursor-pointer',
-                        'dark:text-gray-400' => !$errors->has('file'),
-                        'focus:outline-none',
-                        'dark:bg-gray-700',
-                        'dark:border-gray-600' => !$errors->has('file'),
-                        'dark:placeholder-gray-400' => !$errors->has('file'),
-                        'bg-red-50' => $errors->has('file'),
-                        'border-red-500' => $errors->has('file'),
-                        'text-red-900' => $errors->has('file'),
-                        'placeholder-red-700' => $errors->has('file'),
-                        'focus:ring-red-500' => $errors->has('file'),
-                        'dark:text-red-500' => $errors->has('file'),
-                        'dark:placeholder-red-500' => $errors->has('file'),
-                        'dark:border-red-5000' => $errors->has('file'),
-                    ]) aria-describedby="xlsx_arquive" id="xlsx_arquive"
-                        name="arquivo_dados" type="file" value="{{ old('file') }}">
-                    <p class="mt-1 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">XLSX (Max.
-                        20MB).</p>
-                    @if ($errors->has('file'))
-                        <span class="invalid-feedback">
-                            <strong
-                                class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $errors->first('file') }}</strong>
-                        </span>
-                    @endif
+                            <!--begin::Title-->
+                            <h2 class="card-title align-items-start flex-column">
+                                <span class="card-label fw-bolder text-dark text-2xl font-bold mb-7 dark:text-white">
+                                    Importar Planilha de Livros
+                                </span>
+                                <span class="text-gray-400 mt-1 fw-bold fs-6">Total 2,356 Items in the Stock</span>
+                            </h2>
+                            <!--end::Title-->
+                            <!--begin::Actions-->
+
+                        </div>
+                        <!--end::Card header-->
+                        <!--begin::Card body-->
+                        <div class="card-body ">
+
+                            <form method="post" action="{{ route('admin.importarExcel') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <div class="my-4">
+                                    <label @class([
+                                        'block',
+                                        'mb-2',
+                                        'text-sm',
+                                        'font-medium',
+                                        'dark:text-gray-300' => !$errors->has('file'),
+                                        'text-gray-900' => !$errors->has('file'),
+                                        'text-red-700' => $errors->has('file'),
+                                        'dark:text-red-500' => $errors->has('file'),
+                                    ]) for="user_avatar">Upload
+                                        file</label>
+                                    <input @class([
+                                        'block',
+                                        'w-full',
+                                        'text-sm',
+                                        'bg-gray-50',
+                                        'rounded-lg',
+                                        'border',
+                                        'text-sm',
+                                        'bg-gray-50' => !$errors->has('file'),
+                                        'border-gray-300' => !$errors->has('file'),
+                                        'cursor-pointer',
+                                        'dark:text-gray-400' => !$errors->has('file'),
+                                        'focus:outline-none',
+                                        'dark:bg-gray-700',
+                                        'dark:border-gray-600' => !$errors->has('file'),
+                                        'dark:placeholder-gray-400' => !$errors->has('file'),
+                                        'bg-red-50' => $errors->has('file'),
+                                        'border-red-500' => $errors->has('file'),
+                                        'text-red-900' => $errors->has('file'),
+                                        'placeholder-red-700' => $errors->has('file'),
+                                        'focus:ring-red-500' => $errors->has('file'),
+                                        'dark:text-red-500' => $errors->has('file'),
+                                        'dark:placeholder-red-500' => $errors->has('file'),
+                                        'dark:border-red-5000' => $errors->has('file'),
+                                    ]) aria-describedby="xlsx_arquive"
+                                        id="xlsx_arquive" name="arquivo_dados" type="file"
+                                        value="{{ old('file') }}">
+                                    <p class="mt-1 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">
+                                        XLSX (Max.
+                                        20MB).</p>
+                                    @if ($errors->has('file'))
+                                        <span class="invalid-feedback">
+                                            <strong
+                                                class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $errors->first('file') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button type="submit"
+                                    class="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Enviar
+                                </button>
+                            </form>
+
+                        </div>
+                        <!--end::Card body-->
+                    </div>
                 </div>
-                <button type="submit"
-                    class="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Enviar
-                </button>
-            </form>
+            </div>
 
         </div>
 
     </div>
-
     @isset($notification)
         <div id="toast-success"
             class="flex absolute bottom-5 left-5 items-center p-4 mb-4 w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
